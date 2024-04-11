54313d66cbb07fdfcedac4427fcd0eea22667bfa
ifiokInyang <ifiok.inyang@decagon.dev>
2024-04-11 13:25:05 +0100

    testing automatic merging disabled

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -6,7 +6,7 @@ export default function Courses(){
 
     return (
         <div>
-            <div>New courses</div>
+            <div>New</div>
         <h1>Courses:</h1>
             { store.courses && store.courses.map((course)=>{
             return  <Course course = {course} key={course._id}/>
